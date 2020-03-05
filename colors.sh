5cca15682d90515b792a8e59bdb4e8df446eff9c
TANWEER ALI <tanweeralii908@gmail.com>
2020-03-06 00:00:55 +0530

    Create colors.sh

---
+++ colors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bold=`echo -en "\e[1m"`
+normal=`echo -en "\e[0m"`
+black=`echo -en "\e[30m"`
+red=`echo -en "\e[31m"`
+green=`echo -en "\e[32m"`
+orange=`echo -en "\e[33m"`
+blue=`echo -en "\e[34m"`
+purple=`echo -en "\e[35m"`
+aqua=`echo -en "\e[36m"`
+gray=`echo -en "\e[37m"`
+darkgray=`echo -en "\e[90m"`
+lightred=`echo -en "\e[91m"`
+lightgreen=`echo -en "\e[92m"`
+lightyellow=`echo -en "\e[93m"`
+lightblue=`echo -en "\e[94m"`
+lightpurple=`echo -en "\e[95m"`
+lightaqua=`echo -en "\e[96m"`
+white=`echo -en "\e[97m"`;
